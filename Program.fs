e32f8192a1235dfd71ae2997e8188cd7573bfefc
sabanr <sabanr@users.noreply.github.com>
2020-08-20 22:40:23 -0300

    Added some example types and functions

--- Program.fs
+++ Program.fs
@@ -1,8 +1,38 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+// use records
+type SuccessfulWithdrawal = {
+    Amount : decimal
+    Balance : decimal
+}
+
+type FailedWithdrawal = {
+    Amount: decimal
+    Balance : decimal
+    IsOverdraft: bool
+}
+
+// use discriminated unions to represent data of 1 or more forms
+type WithdrawalResult = 
+    |Success of SuccessfulWithdrawal
+    |InsufficientFunds of FailedWithdrawal
+    |CardExpired of DateTime
+    |UndisclosedFailure
+
+let handleWithdrawal amount =
+    let withdrawalResult = WithdrawalResult.CardExpired(DateTime.Now)
+
+    match withdrawalResult with
+       |Success s -> printfn "Successfully withdrew %f" s.Amount
+       |InsufficientFunds f -> printfn "Failed to withdraw %f. Balance is %f" f.Amount f.Balance
+       |CardExpired ce -> printfn "The card expired on %O" ce
+       |UndisclosedFailure -> printfn "Unknown failure" 
+
+    0
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    handleWithdrawal 10.0
+
+
+
