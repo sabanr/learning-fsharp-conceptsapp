8ea72cc14c24396df95ae89738ec0c48a2889ecd
sabanr <sabanr@users.noreply.github.com>
2020-09-05 18:28:53 -0300

    .

--- Program.fs
+++ Program.fs
@@ -1,38 +1,17 @@
 ﻿open System
-
-// use records
-type SuccessfulWithdrawal = {
-    Amount : decimal
-    Balance : decimal
-}
-
-type FailedWithdrawal = {
-    Amount: decimal
-    Balance : decimal
-    IsOverdraft: bool
-}
-
-// use discriminated unions to represent data of 1 or more forms
-type WithdrawalResult = 
-    |Success of SuccessfulWithdrawal
-    |InsufficientFunds of FailedWithdrawal
-    |CardExpired of DateTime
-    |UndisclosedFailure
-
-let handleWithdrawal amount =
-    let withdrawalResult = WithdrawalResult.CardExpired(DateTime.Now)
-
-    match withdrawalResult with
-       |Success s -> printfn "Successfully withdrew %f" s.Amount
-       |InsufficientFunds f -> printfn "Failed to withdraw %f. Balance is %f" f.Amount f.Balance
-       |CardExpired ce -> printfn "The card expired on %O" ce
-       |UndisclosedFailure -> printfn "Unknown failure" 
-
-    0
+open BasicFunctions
 
 [<EntryPoint>]
 let main argv =
-    handleWithdrawal 10.0
+    let square x = x * x
+    
+    let sumOfSquares n =
+        [1..n]
+        |> List.map square 
+        |> List.sum
 
+    let s = sumOfSquares 100
 
+    printfn "%i" s
 
+    0
